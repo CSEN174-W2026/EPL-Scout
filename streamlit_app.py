c642dabd6f296cc10ed2e5e5be80c190e69f67c4
derek-chui <thatderekchui@gmail.com>
2026-01-14 09:38:12 -0800

    matchday

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,17 @@
+from pathlib import Path
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+ICON = Path(__file__).parent / "images" / "matchday_icon.png"
+LOGO = Path(__file__).parent / "images" / "matchday_logo.png"
+
+st.set_page_config(page_title="Matchday", page_icon=str(ICON), layout="wide")
+
+st.image(str(LOGO), use_container_width=True)
+
+st.title("English Premier League Scouting Dashboard")
+# st.write(
+#     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# )
 
 st.write(
     "cant rush greatness"
